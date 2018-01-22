43345c26c4d510105565d24bcf355cdfdfcbc11a
npappassl <npappas@scottlogic.com>
2018-01-22 16:00:34 +0000

    junit 4 parameterized test in simple solver

--- src/test/java/com/something/to/test/SimpleSolverTest.java
+++ src/test/java/com/something/to/test/SimpleSolverTest.java
@@ -1,68 +1,45 @@
 package com.something.to.test;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
-
+@RunWith(Parameterized.class)
 public class SimpleSolverTest {
-    Solver s;
+    private String input;
+    private int expectedStart;
+    private int expectedLength;
+    private Solver s;
 
-    @Before
-    public void setUp() {
+    public SimpleSolverTest(String input, int expectedStart, int expectedLength){
+        this.input = input;
+        this.expectedStart = expectedStart;
+        this.expectedLength = expectedLength;
+
         s = new SimpleSolver();
+    }
+    @Parameters
+    public static Collection<Object[]> arguments(){
+        return Arrays.asList(new Object[][]{
+                {"aaabbbb", 3 , 4},
+                {"",       -1 , 0},
+                {"a",       0 , 1},
+                {"aa",      0 , 2},
+                {"ab",      0 , 1},
+                {"acvsasvasdddddbasbsaasddbabdbbbbadbadb", 9, 5}
+        });
     }
 
     @Test
     public void findUniformStringTestSimple() {
-        ToTest t = new ToTest("aaabbbb", s);
-        Pair expected = new Pair(3, 4);
-        Pair actual = t.findUniformString();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void findUniformStringTestEmpty() {
-        ToTest t = new ToTest("", s);
-        Pair expected = new Pair(-1, 0);
-        Pair actual = t.findUniformString();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void findUniformStringTestSingleChar() {
-        ToTest t = new ToTest("a", s);
-        Pair expected = new Pair(0, 1);
-        Pair actual = t.findUniformString();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void findUniformStringTestTwoChar() {
-        ToTest t = new ToTest("aa", s);
-        Pair expected = new Pair(0, 2);
-        Pair actual = t.findUniformString();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void findUniformStringTestTwoDistinctsChars() {
-        ToTest t = new ToTest("ab", s);
-        Pair expected = new Pair(0, 1);
-        Pair actual = t.findUniformString();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void findUniformStringTestLong() {
-        ToTest t = new ToTest("acvsasvasdddddbasbsaasddbabdbbbbadbadb", s);
-        Pair expected = new Pair(9, 5);
+        ToTest t = new ToTest(input, s);
+        Pair expected = new Pair(expectedStart, expectedLength);
         Pair actual = t.findUniformString();
 
         assertEquals(expected, actual);
